b883f937dc175e05863ff72a66573a132e7b594b
Carlos Cardoso <carlos.filho@undb.edu.br>
2025-11-18 06:51:13 -0300

    Update .gitignore and db.ts

--- src/server/service/db.ts
+++ src/server/service/db.ts
@@ -2,29 +2,18 @@ import {Pool,Client} from 'pg';
 import { JsonData } from '../util/interfaces';
 import { UUID } from 'crypto';
 import { Entidade } from '../model/entidade';
-import { createClient, RedisClientType } from 'redis';
-
+import config from "../config/db.json";
 
 export class DB {
 
 
     pool:Pool;
     client:Client;
-    redis:RedisClientType;
-    config:JsonData = {
-        host: 'ep-proud-smoke-accvpbt7-pooler.sa-east-1.aws.neon.tech',
-        port: 5432,
-        user: 'neondb_owner',
-        password: 'npg_mzDk09OJIbVU',
-        database: 'neondb',
-        ssl: true
-    };
-
-    constructor(config:JsonData=null){
-        let conf:JsonData = config || this.config;
+    
+    constructor(){
+        let conf: JsonData = config;
         this.pool = new Pool(conf);
-        this.redis = createClient();
-
+        
     }
 
     async query(qr:string):Promise<any>{
